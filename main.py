599deeba0af6fea44b91de58fd2d9a233102a1bb
idekazuki <kuzikaide@gmail.com>
2020-04-16 17:59:39 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+import numpy as np
+import cv2
+import random
+
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+
+
